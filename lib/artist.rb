6ef07cc5de25ba5f3a1c4fe6d2608d662533d807
Veniel Dorestave <veniel@noukod.com>
2018-10-03 14:06:10 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,7 +3,30 @@ class Artist
   attr_accessor :name
 
   def initialize(name)
-    @title = name
+    @titlclass Artist
+  attr_accessor :name
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def add_song(song)
+    song.artist = self
+  end
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
+  end
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+  def self.song_count
+    Song.all.count
+  end
+ende = name
   end
 end
 
