f8cb616376ac0ffd7a2b71fd2dde1e8c98ceb1f6
Veniel Dorestave <veniel@noukod.com>
2018-10-03 14:44:39 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,14 +1,9 @@
 class Post
-  attr_accessor :title, :author
-  @@all = []
-  def initialize(title)
+  attr_accessor :title
+
+  def initialize(title )
     @title = title
-    @@all << self
-  end
-  def self.all
-    @@all
-  end
-  def author_name
-    author.name if author
   end
 end
+ post = Post.new("")
+ post.title = "7/11" 
