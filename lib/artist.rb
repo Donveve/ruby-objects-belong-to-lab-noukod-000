37a15f40d79e0cbd8d84995508d65000c2931023
Veniel Dorestave <veniel@noukod.com>
2018-10-03 14:13:12 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,34 +1,8 @@
 class Artist
-
-  attr_accessor :name
-
-  def initialize(name)
-    @titlclass Artist
-  attr_accessor :name
-  @@all = []
+  attr_accessor :name, :songs
+  
   def initialize(name)
     @name = name
-    @@all << self
-  end
-  def self.all
-    @@all
-  end
-  def add_song(song)
-    song.artist = self
-  end
-  def add_song_by_name(name)
-    song = Song.new(name)
-    song.artist = self
-  end
-  def songs
-    Song.all.select {|song| song.artist == self}
-  end
-  def self.song_count
-    Song.all.count
-  end
-ende = name
+    @songs = song
   end
 end
-
-artist_name = Artist.new("Veniel")
-artist_name.name
