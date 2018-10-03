6b60f99a8e1121ac29c38acdcd73629ce4dc6736
Veniel Dorestave <veniel@noukod.com>
2018-10-03 14:31:39 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,10 @@
 class Artist
-  attr_accessor :name, :songs
+  attr_accessor :name
 
-  def initialize(name = "veniel")
+  def initialize(name )
     @name = name
     @songs = song
   end
 end
-# artist1 = Artist.new("name")
+ artist1 = Artist.new("name")
 # artist1.name
